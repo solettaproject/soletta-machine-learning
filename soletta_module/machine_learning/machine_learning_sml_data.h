06b0f1857ef6436131f978d7f5ed70fcdd03f8f5
Otavio Pontes <otavio.pontes@intel.com>
2015-08-14 11:15:57 -0300

    soletta_module: Create new sml module to handle variables in a single packet
    Receiving inputs and outputs in different packets can lead to
    synchronization issues. If the problem faced requires that sml handles
    all inputs and outputs at the same time and paired, this new nodes,
    fuzzy-sync and neural-network-sync  should be used.
    
    Also add a node called filter-sync, to filter synchronized outputs
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

---
+++ soletta_module/machine_learning/machine_learning_sml_data.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "sol-flow.h"
+#include "sol-flow-packet.h"
+#include "sol-types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct packet_type_sml_data_packet_data {
+    uint16_t inputs_len, outputs_len;
+    struct sol_drange *inputs;
+    struct sol_drange *outputs;
+};
+
+struct packet_type_sml_output_data_packet_data {
+    uint16_t outputs_len;
+    struct sol_drange *outputs;
+};
+
+extern const struct sol_flow_packet_type *PACKET_TYPE_SML_DATA;
+extern const struct sol_flow_packet_type *PACKET_TYPE_SML_OUTPUT_DATA;
+
+struct sol_flow_packet *sml_data_new_packet(struct packet_type_sml_data_packet_data *sml_data);
+int sml_data_send_packet(struct sol_flow_node *src, uint16_t src_port, struct packet_type_sml_data_packet_data *sml_data);
+int sml_data_get_packet(const struct sol_flow_packet *packet, struct packet_type_sml_data_packet_data *sml_data);
+
+struct sol_flow_packet *sml_output_data_new_packet(struct packet_type_sml_output_data_packet_data *sml_output_data);
+int sml_output_data_get_packet(const struct sol_flow_packet *packet, struct packet_type_sml_output_data_packet_data *sml_output_data);
+int sml_output_data_send_packet(struct sol_flow_node *src, uint16_t src_port, struct packet_type_sml_output_data_packet_data *sml_output_data);
+
+
+#ifdef __cplusplus
+}
+#endif
