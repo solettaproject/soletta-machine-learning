c778b3cab5779944c2891408a169d22cfbd62058
Otavio Pontes <otavio.pontes@intel.com>
2015-08-25 15:10:31 -0300

    soletta_module: fix race condition in debug_file process functions
    We need to call sml functions in worker threads and
    sml_set_debug_log_file was being called in main thread. Fixing this
    issue.
    
    This patch also adds the debug_file feature to regular sml nodes.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- soletta_module/machine_learning/machine_learning.c
+++ soletta_module/machine_learning/machine_learning.c
@@ -231,6 +231,7 @@ struct machine_learning_data {
     struct sol_vector output_vec;
     struct sol_vector output_id_vec;
     bool process_needed, predict_needed, send_process_finished, save_needed;
+    char *debug_file;
 
     struct sol_flow_node *node;
     struct sol_worker_thread *worker;
@@ -666,6 +667,7 @@ machine_learning_close(struct sol_flow_node *node, void *data)
         SOL_WRN("Failed to save SML data at:%s", mdata->sml_data_dir);
     sml_free(mdata->sml);
     free(mdata->sml_data_dir);
+    free(mdata->debug_file);
 }
 
 static int
@@ -902,13 +904,21 @@ machine_learning_worker_thread_iterate(void *data)
     process_needed = mdata->process_needed;
     predict_needed = mdata->predict_needed;
     save_needed = mdata->save_needed;
+
+    if (mdata->debug_file) {
+        if (!sml_set_debug_log_file(mdata->sml, mdata->debug_file))
+            SOL_WRN("Failed to set debug log file at : %s", mdata->debug_file);
+        free(mdata->debug_file);
+        mdata->debug_file = NULL;
+    }
+
     pthread_mutex_unlock(&mdata->process_lock);
 
     if (!process_needed && !predict_needed && !save_needed)
         return false;
 
     if (save_needed && !sml_save(mdata->sml, mdata->sml_data_dir))
-        SOL_WRN("Failed to save SML data at:%s", mdata->sml_data_dir);
+        SOL_WRN("Failed to save SML data at: %s", mdata->sml_data_dir);
 
     if ((mdata->run_process && process_needed) || !predict_needed) {
         //Execute process
@@ -1215,6 +1225,35 @@ sml_output_data_send_packet(struct sol_flow_node *src, uint16_t src_port,
     return sol_flow_send_packet(src, src_port, packet);
 }
 
+static int
+debug_file_process(struct sol_flow_node *node, void *data, uint16_t port,
+    uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    int r;
+    struct machine_learning_data *mdata = data;
+    const char *str;
+
+    r = sol_flow_packet_get_string(packet, &str);
+    SOL_INT_CHECK(r, < 0, r);
+
+    r = mutex_lock(&mdata->process_lock);
+    SOL_INT_CHECK(r, < 0, r);
+
+    if (mdata->debug_file)
+        free(mdata->debug_file);
+
+    if (str)
+        mdata->debug_file = strdup(str);
+    else {
+        mdata->debug_file = malloc(1);
+        mdata->debug_file[0] = 0;
+    }
+
+    pthread_mutex_unlock(&mdata->process_lock);
+
+    return 0;
+}
+
 struct machine_learning_sync_data {
     //Used only in open method or/and worker thread. No need to lock
     struct sml_object *sml;
@@ -1230,6 +1269,7 @@ struct machine_learning_sync_data {
     //Used by main thread and process thread. Need to be locked
     struct sol_ptr_vector input_queue;
     struct sol_ptr_vector output_queue;
+    char *debug_file;
     bool save_needed;
 
     struct sol_worker_thread *worker;
@@ -1350,6 +1390,13 @@ machine_learning_sync_worker_thread_iterate(void *data)
 
     r = mutex_lock(&mdata->queue_lock);
     SOL_INT_CHECK(r, < 0, false);
+
+    if (mdata->debug_file) {
+        if (!sml_set_debug_log_file(mdata->sml, mdata->debug_file))
+            SOL_WRN("Failed to set debug log file at : %s", mdata->debug_file);
+        free(mdata->debug_file);
+        mdata->debug_file = NULL;
+    }
 
     if (mdata->save_needed && !sml_save(mdata->sml, mdata->sml_data_dir))
         SOL_WRN("Failed to save the SML data at:%s", mdata->sml_data_dir);
@@ -1477,6 +1524,7 @@ machine_learning_sync_close(struct sol_flow_node *node, void *data)
     sol_ptr_vector_clear(&mdata->output_queue);
     free(mdata->output_steps);
     free(mdata->sml_data_dir);
+    free(mdata->debug_file);
 }
 
 static int
@@ -1767,9 +1815,21 @@ sml_data_debug_file(struct sol_flow_node *node, void *data, uint16_t port,
 
     r = sol_flow_packet_get_string(packet, &str);
     SOL_INT_CHECK(r, < 0, r);
-    SOL_NULL_CHECK(str, -EINVAL);
 
-    sml_set_debug_log_file(mdata->sml, str);
+    r = mutex_lock(&mdata->queue_lock);
+    SOL_INT_CHECK(r, < 0, r);
+
+    if (mdata->debug_file)
+        free(mdata->debug_file);
+
+    if (str)
+        mdata->debug_file = strdup(str);
+    else {
+        mdata->debug_file = malloc(1);
+        mdata->debug_file[0] = 0;
+    }
+
+    pthread_mutex_unlock(&mdata->queue_lock);
 
     return 0;
 }
